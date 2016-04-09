efe65c3c106c86fc2de00397dc879b1e9de8c244
linrea <234811990@qq.com>
2016-04-09 16:18:27 -0400

    Added files via upload

---
+++ UniquePaths.java
@@ -0,0 +1,32 @@
+package leetcode;
+/*
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+ */
+public class UniquePaths
+{
+	public static int uniquePaths(int m, int n)
+	{
+		if(m==0&n==0) return 0;
+		int[][] dp=new int[m][n];
+		for(int i=0;i<m;i++)
+			dp[i][0]=1;
+		for(int i=1;i<n;i++)
+			dp[0][i]=1;
+		for(int i=1;i<m;i++)
+			for(int j=1;j<n;j++)
+			{
+				dp[i][j]=dp[i-1][j]+dp[i][j-1];
+			}
+		return dp[m-1][n-1];
+		
+	}
+	public static void main(String[] args)
+	{
+		int m=1,n=1;
+		int a=uniquePaths(m,n);
+		System.out.println(a);
+	}
+
+}
